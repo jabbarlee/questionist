ca2c532e64877363368b53c9935854a0630a7adc
“Amil <amiljabbarlee@gmail.com>
2024-09-28 21:23:09 -0500

    navbar added to layout

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Navbar from '@/components/ui/Navbar'
 
 export const metadata: Metadata = {
   title: "Questionist",
@@ -13,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar/>
         {children}
       </body>
     </html>
