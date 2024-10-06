5f1cb77be16b633ac9e237c486dfcf2ad0812b6a
“Amil <amiljabbarlee@gmail.com>
2024-10-05 21:37:00 -0500

    HeadingText added to LoginForm

--- src/components/pages/Signin/LoginForm/index.tsx
+++ src/components/pages/Signin/LoginForm/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import InputWrapper from '@/components/pages/Signin/InputWrapper'
 import styles from './index.module.css'
 import SubText from '@/components/pages/Signin/SubText'
+import HeadingText from '@/components/pages/Signin/HeadingText'
 
 export default function index() {
   return (
     <div className={styles.loginForm}>
+      <HeadingText />
       <InputWrapper />
       <SubText />
     </div>
