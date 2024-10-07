1c42811e86f566d8da925225bed8573a8e30e77f
“Amil <amiljabbarlee@gmail.com>
2024-10-07 17:21:12 -0500

    SignInPage bug fixes

--- src/components/pages/Signin/LoginForm/index.tsx
+++ src/components/pages/Signin/LoginForm/index.tsx
@@ -3,7 +3,6 @@ import InputWrapper from '@/components/pages/Signin/InputWrapper'
 import styles from './index.module.css'
 import SubText from '@/components/pages/Signin/SubText'
 import HeadingText from '@/components/pages/Signin/HeadingText'
-import ProvidersWrapper from '@/components/pages/Signin/ProvidersWrapper'
 
 export default function index({ ...props }) {
   return (
