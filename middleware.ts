d4705b9410aeae9266b246fbdf98ef7ff1b94aa7
“Amil <amiljabbarlee@gmail.com>
2024-10-15 08:19:38 -0500

    middleware created

---
+++ middleware.ts
@@ -0,0 +1,5 @@
+import { auth } from '@/config/firebase';
+
+export default async function middleware(request: Request) {
+    
+}
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Auth
 export const auth = getAuth(app);
+export const db = getFirestore(app)
