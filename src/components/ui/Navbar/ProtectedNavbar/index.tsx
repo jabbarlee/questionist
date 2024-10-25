8b9742e8bd8933fb5598ce6e7ad4b6770e8f61c7
“Amil <amiljabbarlee@gmail.com>
2024-10-25 16:55:32 -0500

    Navbar integrated

--- src/components/ui/Navbar/ProtectedNavbar/index.tsx
+++ src/components/ui/Navbar/ProtectedNavbar/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import styles from './index.module.css'
+import NavbarLink from '@/components/ui/NavbarLink';
+import Text from '@/components/ui/Text';
 
 export default function index() {
   return (
-    <div>
-        
+    <div className={styles.navbar}>
+        <div>
+            Logo
+        </div>
+        <div className={styles.linksWrapper}>
+            <Text subheading={true}>Home</Text>
+            <NavbarLink redirect='/'>Dashboard</NavbarLink>
+            <NavbarLink redirect='/'>Practice</NavbarLink>
+            <NavbarLink redirect='/'>My Results</NavbarLink>
+            <Text subheading={true}>Settings</Text>
+            <NavbarLink redirect='/'>Profile</NavbarLink>
+            <NavbarLink redirect='/'>Plans</NavbarLink>
+        </div>
     </div>
   )
 }
