237f12c4453f8383b1e2004bc8c290984d6e164b
“Amil <amiljabbarlee@gmail.com>
2024-11-24 10:35:26 -0600

    welcome banner refactored

--- src/components/ui/_testing/WelcomeBanner/index.tsx
+++ src/components/ui/_testing/WelcomeBanner/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './index.module.css'
 import Typography from "@mui/material/Typography";
+import Link from 'next/link'
 
 export default function Index({
     fullName,
@@ -14,6 +15,9 @@ export default function Index({
             <div className={styles.welcomeTextWrapper}>
                 <Typography className={styles.welcomeText}>Welcome back {fullName}</Typography>
                 <Typography className={styles.welcomeSubText}>{email}</Typography>
+            </div>
+            <div className={styles.editButtonWrapper}>
+                <Link href='/settings' className={styles.editButton}>Edit</Link>
             </div>
         </div>
     );
