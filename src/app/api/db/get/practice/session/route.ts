6628acf7331eb40a822da273b62b534f6a2dcab4
“Amil <amiljabbarlee@gmail.com>
2024-12-16 15:58:20 -0600

    practice session endpoint created

---
+++ src/app/api/db/get/practice/session/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { fetchPracticeSessionConfig} from "@/actions/firebase/getDoc";
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const sessionId = searchParams.get("sessionId");
+
+    if(!sessionId){
+        return NextResponse.json({ error: 'No session ID provided' }, { status: 401 });
+    }
+
+    try{
+        const sessionData = await fetchPracticeSessionConfig(sessionId);
+
+        if(!sessionData){
+            return NextResponse.json({ error: 'No session data found' }, { status: 404 });
+        }
+
+        return NextResponse.json(sessionData);
+    }catch(error){
+        return NextResponse.json({ error: error }, { status: 500 });
+    }
+}
