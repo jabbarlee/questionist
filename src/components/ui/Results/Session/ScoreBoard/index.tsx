4dd86c6fe292ac382993023219d2b63fab6b45b3
“Amil <amiljabbarlee@gmail.com>
2024-12-22 13:58:09 -0600

    ScoreBoard redesigned

--- src/components/ui/Results/Session/ScoreBoard/index.tsx
+++ src/components/ui/Results/Session/ScoreBoard/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./index.module.css";
-import { ScoreChip } from "./ScoreChip";
+import { ScoreChip, ScoreProgress } from "./ScoreChip";
+import {Typography} from "@mui/material";
+import {Button} from "antd";
 
 export default function ScoreBoard({
     correctAnswers,
@@ -17,39 +19,24 @@ export default function ScoreBoard({
 }) {
     return (
         <div className={styles.scoreBoard}>
-            <h2 className={styles.title}>Stats</h2>
-            <div className={styles.scoreBoardWrapper}>
-                <div className={styles.statsContainer}>
-                    <ScoreChip success>{correctAnswers}</ScoreChip>
-                    <ScoreChip error>{incorrectAnswers}</ScoreChip>
+            <div className={styles.passedTextContainer}>
+                <Typography className={styles.passedText}>
+                    {overallScore >= 80 ? "You passed the test!" : "You need more practice!"}
+                </Typography>
+                <Button type={"link"} color={"primary"} size={"large"}>
+                    Practice again
+                </Button>
+            </div>
 
-                    <div className={styles.statBox}>
-                        <p className={styles.statLabel}>Timer</p>
-                        <p className={styles.statValue}>{timerStat || '21:20'}</p>
-                    </div>
+            <div className={styles.chipContainer}>
+                <div className={styles.chipWrapper}>
+                    <ScoreChip success text={"Correct"}>{correctAnswers}</ScoreChip>
+                    <ScoreChip error text={"Incorrect"}>{incorrectAnswers}</ScoreChip>
+                    <ScoreChip text={"Total"}>{numOfQuestions}</ScoreChip>
+                    <ScoreChip text={"Omitted"}>{1}</ScoreChip>
                 </div>
-                <div className={styles.overallScoreWrapper}>
-                    <div>
-                        <p className={styles.overallScoreLabel}>Overall Score</p>
-                    </div>
-                    <div className={styles.arcWrapper}>
-                        <svg className={styles.arc} viewBox="0 0 36 36">
-                            <path
-                                className={styles.arcBackground}
-                                d="M18 2 A16 16 0 1 1 17.99 2"
-                            />
-                            <path
-                                className={styles.arcProgress}
-                                d="M18 2 A16 16 0 1 1 17.99 2"
-                                style={{
-                                    strokeDasharray: `${(overallScore / 100) * 100} 100`,
-                                }}
-                            />
-                        </svg>
-                        <div className={styles.arcText}>
-                            <span>{overallScore}%</span>
-                        </div>
-                    </div>
+                <div className={styles.chipWrapper}>
+                    <ScoreProgress progress={80}/>
                 </div>
             </div>
         </div>
