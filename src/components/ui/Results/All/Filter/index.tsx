9587c3c81a2832955510b96c2c03f83046449c30
“Amil <amiljabbarlee@gmail.com>
2024-12-30 10:08:06 -0600

    points system integrated

---
+++ src/components/ui/Results/All/Filter/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './index.module.css';
+import { SessionData } from '@/types';
+
+const Filter = () => {
+
+    return (
+        <div className={styles.card}>
+
+        </div>
+    );
+};
+
+export default Filter;
