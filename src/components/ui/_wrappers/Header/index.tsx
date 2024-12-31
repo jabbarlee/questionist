a2b2eb786e1e4a09b340e2cf57770f295ad6ad39
“Amil <amiljabbarlee@gmail.com>
2024-12-31 13:09:20 -0600

    Header component redesigned for responsiveness

--- src/components/ui/_wrappers/Header/index.tsx
+++ src/components/ui/_wrappers/Header/index.tsx
@@ -1,10 +1,45 @@
+"use client"
+
 import React from 'react'
 import styles from './index.module.css'
+import { Dropdown } from 'antd'
+import { MenuProps } from 'antd/lib/menu'
 
 export default function ({ children } : { children: React.ReactNode }) {
+    const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log('click left button', e);
+    };
+
+    const handleMenuClick: MenuProps['onClick'] = (e) => {
+        console.log('click', e);
+    };
+
+    const items: MenuProps['items'] = [
+        {
+            label: '1st menu item',
+            key: '1',
+        },
+        {
+            label: '2nd menu item',
+            key: '2',
+        },
+    ];
+
+    const menuProps = {
+        items,
+        onClick: handleMenuClick,
+    };
+
   return (
       <div className={styles.header}>
-          <p className={styles.headerText}>{children}</p>
+          <div>
+              <p className={styles.headerText}>{children}</p>
+          </div>
+          <div>
+              <Dropdown.Button menu={menuProps} onClick={handleButtonClick}>
+                  amiljabbarlee@gmail.com
+              </Dropdown.Button>
+          </div>
       </div>
   )
 }
