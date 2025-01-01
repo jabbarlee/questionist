70954733061d82bb311f9146c4352fdcb6c4ee77
“Amil <amiljabbarlee@gmail.com>
2024-12-31 21:37:24 -0600

    drowpdown integrated with db

--- src/actions/firebase/getDoc.ts
+++ src/actions/firebase/getDoc.ts
@@ -107,3 +107,28 @@ export const getAllResults = async () => {
         return null;
     }
 };
+
+export const getUser = async () => {
+    try {
+        const response = await fetch('/api/firebase/get/user');
+        const { uid } = await response.json();
+
+        if (!uid) {
+            console.error("User ID is missing");
+            return null;
+        }
+
+        const userRef = doc(db, 'users', uid);
+        const userSnap = await getDoc(userRef);
+
+        if (userSnap.exists()) {
+            return userSnap.data();
+        } else {
+            console.error('No such document!');
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching user data: ", error);
+        return null;
+    }
+}
--- src/components/ui/_wrappers/Header/index.tsx
+++ src/components/ui/_wrappers/Header/index.tsx
@@ -1,13 +1,26 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './index.module.css'
 import { Dropdown } from 'antd'
 import { MenuProps } from 'antd/lib/menu'
+import { getUser } from '@/actions/firebase/getDoc'
+import { useRouter } from "next/navigation"
+import { Button } from "antd"
+import Link from "next/link"
 
 export default function ({ children } : { children: React.ReactNode }) {
+    const [user, setUser] = useState<any>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        getUser().then((data) => {
+            setUser(data?.email);
+        });
+    }, []);
+
     const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log('click left button', e);
+        router.push('/settings');
     };
 
     const handleMenuClick: MenuProps['onClick'] = (e) => {
@@ -16,30 +29,43 @@ export default function ({ children } : { children: React.ReactNode }) {
 
     const items: MenuProps['items'] = [
         {
-            label: '1st menu item',
+            label: (
+                <div>
+                    <Link
+                        href={"/settings"}
+                        className={styles.link}
+                    >
+                        Settings
+                    </Link>
+                </div>
+            ),
             key: '1',
         },
         {
-            label: '2nd menu item',
+            label: (
+                <div>
+                    <Link
+                        href={"/logout"}
+                        className={styles.link}
+                    >
+                        Logout
+                    </Link>
+                </div>
+            ),
             key: '2',
         },
     ];
 
-    const menuProps = {
-        items,
-        onClick: handleMenuClick,
-    };
-
-  return (
-      <div className={styles.header}>
-          <div>
-              <p className={styles.headerText}>{children}</p>
-          </div>
-          <div>
-              <Dropdown.Button menu={menuProps} onClick={handleButtonClick}>
-                  amiljabbarlee@gmail.com
-              </Dropdown.Button>
-          </div>
-      </div>
-  )
+    return (
+        <div className={styles.header}>
+            <div>
+                <p className={styles.headerText}>{children}</p>
+            </div>
+            <div>
+                <Dropdown menu={{ items }} placement="bottomRight">
+                    <Button>{user ? user : 'Loading...'}</Button>
+                </Dropdown>
+            </div>
+        </div>
+    )
 }
