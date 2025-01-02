cd4356ba8c64b3d52f8e425159d7de3cbfd5ff9d
“Amil <amiljabbarlee@gmail.com>
2025-01-01 19:40:34 -0600

    bug fixes

--- src/components/ui/Results/All/Table/index.tsx
+++ src/components/ui/Results/All/Table/index.tsx
@@ -106,6 +106,7 @@ export default function ResultsTable({ sessions, showModal }: TableProps) {
                                 style={{width: `${overallScore}%`}}
                             />
                         </div>
+
                     </div>
                 );
             },
