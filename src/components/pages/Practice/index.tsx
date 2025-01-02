a12306fda5d3538c767f2b4822302f8fd169b4fd
“Amil <amiljabbarlee@gmail.com>
2025-01-01 21:12:19 -0600

    added HowItWorks section to practice page

--- src/components/pages/Practice/index.tsx
+++ src/components/pages/Practice/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './index.module.css'
 import Main from "@/components/ui/_wrappers/Main";
-import WelcomeBanner from '@/components/ui/Practice/WelcomeBanner'
+import { Tag } from 'antd';
 import SloganBanner from '@/components/ui/Practice/SloganBanner'
 import CustomSet from '@/components/ui/Practice/CustomSet'
 import Header from "@/components/ui/_wrappers/Header";
 import Page from "@/components/ui/_wrappers/Page";
+import Typography from "@mui/material/Typography";
 
 export default function index() {
     return (
@@ -14,9 +15,42 @@ export default function index() {
                 Practice makes it perfect
             </Header>
             <Main>
-                <WelcomeBanner fullName={'Amil Jabarli'} email={'amiljabbarlee@gmail.com'}/>
-                <CustomSet/>
-                <SloganBanner/>
+                <div className={styles.wrapper}>
+                    <div className={styles.widthWrapper}>
+                        <CustomSet/>
+                    </div>
+                    <div className={styles.widthWrapper}>
+                        <div className={styles.stepsWrapper}>
+                            <Typography className={styles.stepHeadingText}>How It Works</Typography>
+                            <div className={styles.stepsContainer}>
+                                <div className={styles.step}>
+                                    <Tag color="blue" className={styles.stepTag}>
+                                        Earn rewards
+                                    </Tag>
+                                    <div className={styles.stepContent}>
+                                        <p className={styles.stepSubText}>As you progress you will gain more points which you can use later</p>
+                                    </div>
+                                </div>
+                                <div className={styles.step}>
+                                    <Tag color="green" className={styles.stepTag}>
+                                        AI-Powered Questions
+                                    </Tag>
+                                    <div className={styles.stepContent}>
+                                        <p className={styles.stepSubText}>Each set is customized for your own needs</p>
+                                    </div>
+                                </div>
+                                <div className={styles.step}>
+                                    <Tag color="purple" className={styles.stepTag}>
+                                        Track Progress
+                                    </Tag>
+                                    <div className={styles.stepContent}>
+                                        <p className={styles.stepSubText}>See detailed analytics and performance trends over time.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Main>
         </Page>
     )
