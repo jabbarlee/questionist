860469a4e1b52d6c389f0196320663f416ea8ef2
“Amil <amiljabbarlee@gmail.com>
2025-01-21 23:06:09 -0600

    streak progress container created

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './index.module.css'
 import ScoreChart from '@/components/ui/Dashboard/ScoreChart'
 import MessageContainer from '@/components/ui/Dashboard/MessageContainer'
+import StreakProgress from '@/components/ui/Dashboard/StreakProgress'
 import Typography from "@mui/material/Typography";
 
 export default function Dashboard() {
@@ -10,6 +11,9 @@ export default function Dashboard() {
             <div className={styles.messageContainer}>
                 <div>
                     <MessageContainer/>
+                </div>
+                <div>
+                    <StreakProgress/>
                 </div>
             </div>
             <div className={styles.chartContainer}>
