4bf864d01df5af310065d39c7034d3e69925c2d7
“Amil <amiljabbarlee@gmail.com>
2025-01-28 10:42:41 -0600

    streak component removed

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './index.module.css'
 import ScoreChart from '@/components/ui/Dashboard/ScoreChart'
 import MessageContainer from '@/components/ui/Dashboard/MessageContainer'
-import StreakProgress from '@/components/ui/Dashboard/StreakProgress'
 import Typography from "@mui/material/Typography";
 
 export default function Dashboard() {
@@ -11,9 +10,6 @@ export default function Dashboard() {
             <div className={styles.messageContainer}>
                 <div>
                     <MessageContainer/>
-                </div>
-                <div>
-                    <StreakProgress/>
                 </div>
             </div>
             <div className={styles.chartContainer}>
