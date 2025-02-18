f9d8526e84c39a2ff61c305c7e937f3fee13d6d7
Amil Jabarli <amiljabbarlee@gmail.com>
2025-02-18 15:38:27 -0600

    Remove unused Card component from Practice page to streamline layout

--- src/components/pages/Practice/index.tsx
+++ src/components/pages/Practice/index.tsx
@@ -128,26 +128,6 @@ export default function index() {
                         </div>
                     </Card>
                 </div>
-                <Card
-                    heading={'Premium practice sets'}
-                    subHeading={'Start practicing with premium sets'}
-                >
-                    <Button
-                        size="large"
-                        type="primary"
-                        href="/practice/create"
-                        style={{ textDecoration: 'none', width: '50%' }}
-                    >
-                        See pricing
-                    </Button>
-                    <Button
-                        size="large"
-                        type="default"
-                        style={{ width: '50%' }}
-                    >
-                        Why premium?
-                    </Button>
-                </Card>
             </Main>
         </Page>
     )
