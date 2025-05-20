7849fb474716674296bcae21c5bf2739a70d0769
Amil Jabarli <amiljabbarlee@gmail.com>
2025-05-20 15:23:02 +0400

    Remove unused Firebase authentication functions and query client; add useCurrentUser hook for fetching current user data

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,10 +1,8 @@
-// layout.tsx
-
-import styles from './layout.module.css';
-import '../(web)/globals.css';
-import type { Metadata } from 'next'
-import VerticalNavbar from '@/components/ui/Navbar/VerticalNavbar';
-import { inter } from '@/data/fonts';
+import styles from "./layout.module.css";
+import "../(web)/globals.css";
+import type { Metadata } from "next";
+import VerticalNavbar from "@/components/ui/Navbar/VerticalNavbar";
+import { inter } from "@/data/fonts";
 
 export const metadata: Metadata = {
   title: "Questionist",
@@ -12,22 +10,20 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
- children,
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-      <html lang="en">
-        <body className={inter.className}>
-          <div className={styles.pageWrapper}>
-            <aside className={styles.sidebar}>
-              <VerticalNavbar />
-            </aside>
-            <main className={styles.contentWrapper}>
-              {children}
-            </main>
-          </div>
-        </body>
-      </html>
+    <html lang="en">
+      <body className={inter.className}>
+        <div className={styles.pageWrapper}>
+          <aside className={styles.sidebar}>
+            <VerticalNavbar />
+          </aside>
+          <main className={styles.contentWrapper}>{children}</main>
+        </div>
+      </body>
+    </html>
   );
 }
