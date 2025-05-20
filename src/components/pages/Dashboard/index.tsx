faf53c08d85f46f6983b671fe57ed40c78c6c78a
Amil Jabarli <amiljabbarlee@gmail.com>
2025-05-20 20:31:00 +0400

    Refactor Dashboard component: standardize import formatting and improve code readability

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import styles from './index.module.css'
-import ScoreChart from '@/components/ui/Dashboard/ScoreChart'
-import MessageContainer from '@/components/ui/Dashboard/MessageContainer'
+import React from "react";
+import styles from "./index.module.css";
+import ScoreChart from "@/components/ui/Dashboard/ScoreChart";
+import MessageContainer from "@/components/ui/Dashboard/MessageContainer";
 import Typography from "@mui/material/Typography";
-import { TrendingContract } from '@/components/ui/Dashboard/TrendingContract';
+import { TrendingContract } from "@/components/ui/Dashboard/TrendingContract";
 
 export default function Dashboard() {
-    return (
-        <div className={styles.pageWrapper}>
-            <div className={styles.leftContainer}>
-                <div className={styles.messageContainer}>
-                    <MessageContainer/>
-                </div>
-                <div className={styles.trendingContractContainer}>
-                    <TrendingContract/>
-                </div>
-            </div>
-            <div className={styles.chartContainer}>
-                <Typography className={styles.heading} fontSize={'22px'}>Scores</Typography>
-                <ScoreChart/>
-            </div>
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.leftContainer}>
+        <div className={styles.messageContainer}>
+          <MessageContainer />
         </div>
-    )
+        <div className={styles.trendingContractContainer}>
+          <TrendingContract />
+        </div>
+      </div>
+      <div className={styles.chartContainer}>
+        <Typography className={styles.heading} fontSize={"22px"}>
+          Scores
+        </Typography>
+        <ScoreChart />
+      </div>
+    </div>
+  );
 }
