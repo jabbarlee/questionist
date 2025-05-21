46174802dac5631cf5bfc043452cbcdd75c1ff83
Amil Jabarli <amiljabbarlee@gmail.com>
2025-05-21 11:43:33 +0400

    Refactor updateQuestions function: improve code readability and formatting in updateQuestions.ts

--- src/actions/firebase/update/updateQuestions.ts
+++ src/actions/firebase/update/updateQuestions.ts
@@ -1,63 +1,61 @@
-import { doc, updateDoc, getDoc } from 'firebase/firestore';
-import { db } from '@/config/firebase';
-import { QuestionProps, SelectedOption, SessionData } from '@/types';
-import { getResults } from '@/actions/firebase/get/getResults';
+import { doc, updateDoc, getDoc } from "firebase/firestore";
+import { db } from "@/config/firebase";
+import { QuestionProps, SelectedOption, SessionData } from "@/types";
 
 type SessionTimingInfo = {
-    sessionStartTime: string,
-    sessionFinishedTime: string,
-    sessionDuration: {
-        minutes: number,
-        seconds: number
-    },
-    sessionRemainingTime: {
-        minutes: number,
-        seconds: number
-    }
-}
+  sessionStartTime: string;
+  sessionFinishedTime: string;
+  sessionDuration: {
+    minutes: number;
+    seconds: number;
+  };
+  sessionRemainingTime: {
+    minutes: number;
+    seconds: number;
+  };
+};
 
 export async function updateQuestions(
-    sessionId: string,
-    questions: QuestionProps[],
-    selectedChoices: SelectedOption[],
-    sessionTimingInfo: SessionTimingInfo,
+  sessionId: string,
+  questions: QuestionProps[],
+  selectedChoices: SelectedOption[],
+  sessionTimingInfo: SessionTimingInfo
 ) {
-    const response = await fetch('/api/firebase/get/user');
-    const { uid } = await response.json();
+  const response = await fetch("/api/firebase/get/user");
+  const { uid } = await response.json();
 
-    if (questions.length !== selectedChoices.length) {
-        return { success: false, error: 'You have not finished all the questions' };
-    }
+  if (questions.length !== selectedChoices.length) {
+    return { success: false, error: "You have not finished all the questions" };
+  }
 
-    if (questions.length === 0) {
-        return { success: false, error: 'Session was not found' };
-    }
+  if (questions.length === 0) {
+    return { success: false, error: "Session was not found" };
+  }
 
-    const questionsToUpdate = questions.map((question, index) => ({
-        type: "multiple-choice",
-        questionText: question.question,
-        choices: question.choices.map((choice) => choice.text),
-        correctAnswer: question.correctAnswer,
-        selectedChoice: selectedChoices[index]?.selectedOptionText || null,
-    }));
+  const questionsToUpdate = questions.map((question, index) => ({
+    type: "multiple-choice",
+    questionText: question.question,
+    choices: question.choices.map((choice) => choice.text),
+    correctAnswer: question.correctAnswer,
+    selectedChoice: selectedChoices[index]?.selectedOptionText || null,
+  }));
 
-    try {
-        // Reference the Firestore document
-        const sessionDocRef = doc(db, 'users', uid, 'practiceSessions', sessionId);
+  try {
+    // Reference the Firestore document
+    const sessionDocRef = doc(db, "users", uid, "practiceSessions", sessionId);
 
-        // Update the Firestore document
-        await updateDoc(sessionDocRef, {
-            questions: questionsToUpdate,
-            updatedAt: new Date().toISOString(),
-            sessionTimingInfo: sessionTimingInfo,
-        });
+    // Update the Firestore document
+    await updateDoc(sessionDocRef, {
+      questions: questionsToUpdate,
+      updatedAt: new Date().toISOString(),
+      sessionTimingInfo: sessionTimingInfo,
+    });
 
-        // const { success } = await getResults(sessionId);
+    // const { success } = await getResults(sessionId);
 
-        return { success: true, message: 'Session finished' };
-
-    } catch (error) {
-        console.error('Error updating questions in Firestore:', error);
-        return { success: false, error: 'Failed to submit your session.' };
-    }
+    return { success: true, message: "Session finished" };
+  } catch (error) {
+    console.error("Error updating questions in Firestore:", error);
+    return { success: false, error: "Failed to submit your session." };
+  }
 }
